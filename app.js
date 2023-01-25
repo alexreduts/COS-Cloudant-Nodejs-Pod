229cb70fd2d4f074fd56371c522c19a5dd002e8a
Alex Studer <Alex.Studer@ibm.com>
2023-01-25 15:33:49 +0100

    Document creation demo added

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const app = express()
 const { CloudantV1 } = require('@ibm-cloud/cloudant')
 const port = 8080
 
+const productsDoc: CloudantV1.Document = {
+  _id: 'small-appliances:1000042',
+  type: 'product',
+  productid: '1000042',
+  brand: 'Salter',
+  name: 'Digital Kitchen Scales',
+  description: 'Slim Colourful Design Electronic Cooking Appliance for Home / Kitchen, Weigh up to 5kg + Aquatronic for Liquids ml + fl. oz. 15Yr Guarantee - Green',
+  price: 14.99,
+  image: 'assets/img/0gmsnghhew.jpg'
+};
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -22,11 +33,19 @@ app.get('/cloudant', (req, res) => {
   
   cloudantDemo.getServerInformation()
     .then(response => {
-      console.log(`Cloudant Readout ${response.result}`)
+      console.log(`Cloudant Readout`)
+      console.log(response.result)
     })
     .catch(error => {
       console.log(`Error Message:\n ${error.message}`)
     })
+
+  cloudantDemo.postDocument({
+      db: 'cloudant-demo-db',
+      document: productsDoc
+    }).then(response => {
+      console.log(`Document should have been added ${response.result}`);
+    });
 
   res.send(`Welcome have a look at the pod log`)
 })
